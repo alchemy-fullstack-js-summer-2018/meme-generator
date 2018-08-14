0d2358300481e88d1a8f3854136990e2a897f231
Rehmsy <sarahrehmer@gmail.com>
2018-08-14 16:45:05 -0700

    build working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 const { resolve } = require('path');
-//const CleanPlugin = require('clean-webpack-plugin');
-//const HtmlPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
