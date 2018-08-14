6112bda23d2a09f178bf7e91b5661bd31d0caba9
MariahAdams <mariah.adams23@yahoo.com>
2018-08-14 16:15:47 -0700

    basic build setup successful!, bundle.js file added from webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+const { resolve } = require('path');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path, 
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development'
+}
