19de4b161c9d2747e10e14e986d5f55aaafb36af
MariahAdams <mariah.adams23@yahoo.com>
2018-08-14 16:30:37 -0700

    added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const { resolve } = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
@@ -14,6 +15,7 @@ module.exports = {
   },
   mode: 'development',
   plugins: [
-    new CleanPlugin(`${path}/bundle.*.js`)
+    new CleanPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
   ]
 }
