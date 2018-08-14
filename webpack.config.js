9206f296ad9c0eec59ecfec82d04ae702f3fd55e
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-14 16:11:17 -0700

    Add webpack packages and create initial build setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const { resolve } = require('path');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `./${buildDir}`
+  },
+  plugins: [
+
+  ]
+}
