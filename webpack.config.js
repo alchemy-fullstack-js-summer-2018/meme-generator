624e3f04d6024e867dbd2324ee3760bf35207dbb
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-14 16:36:42 -0700

    Created bundle and updated gitignore

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+const { resolve } = require('path');
+// const CleanPlugin = require('clean-webpack-plugin');
+// const HtmlPlugin = require('html--webpack-plugin');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+
+
+ 
+}
