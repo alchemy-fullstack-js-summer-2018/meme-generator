bf9425f7f03b1e402cf6fdc12ab02cac5db2c235
Rehmsy <sarahrehmer@gmail.com>
2018-08-14 16:28:10 -0700

    basic build complete

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+const { resolve } = require('path');
+//const CleanPlugin = require('clean-webpack-plugin');
+//const HtmlPlugin = require('html-webpack-plugin');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  // start here
+  entry: './src/index.js',
+  // put the build output here (not dev server)
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  // mode (will eventually be cmd line arg in package.json scripts)
+  mode: 'development'
+};
