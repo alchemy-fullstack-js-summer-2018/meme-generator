87f8ba78c41e6875f3f4622f12961c500846faaa
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-14 16:22:57 -0700

    Install HTMLWebpack plugin and verify it works

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
@@ -18,5 +19,6 @@ module.exports = {
   },
   plugins: [
     new CleanPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
   ],
 }
