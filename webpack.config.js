8947e6e2bb3faab80b1cb0cdf7d7ab9cf17ea840
MariahAdams <mariah.adams23@yahoo.com>
2018-08-14 16:56:26 -0700

    added react options to presets

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
                 targets: {
                   browsers: 'Chrome 65'
                 }
-              }]
+              }],
+              'react'
             ],
             plugins: [
               require('babel-plugin-transform-object-rest-spread'),
