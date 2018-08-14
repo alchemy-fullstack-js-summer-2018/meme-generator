100a4d941bad3ac684127e5371466990d2da15bf
Easton Gorishek <ieaston26@gmail.com>
2018-08-14 16:54:26 -0700

    react added

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
                   browsers: 'Chrome 65'
                 },
               }],
+              'react'
             ],
             plugins: [
               require('babel-plugin-transform-object-rest-spread'),
