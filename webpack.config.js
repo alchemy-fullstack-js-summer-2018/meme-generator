0dac491518695a0d458cd448f67fe59c9ca150a6
Mario Quintana <pyrros2000@gmail.com>
2018-08-14 16:16:24 -0700

    created entry point and output in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,12 @@ const { resolve } = require('path');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path,
+        filename: 'bundle.[hash].js',
+        publicPath: ''
+    },
+}
