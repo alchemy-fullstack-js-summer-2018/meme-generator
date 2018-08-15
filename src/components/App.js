82b9e526e1bd3c7d214f4f75d487361984caa5bb
Mario Quintana <pyrros2000@gmail.com>
2018-08-15 08:30:12 -0700

    Created basic jsx layout for html pag, need to create functhat are called within the jsx for anything to render

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import cowsay from 'cowsay-browser';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
+import styles from './App.css';
 
 class App extends Component {
+
+    state = {
+        content: 'Helllooo!',
+        cows: [],
+        cow: 'default'
+    };
+
+    handleCowSelect = (cow = 'default') => {
+        this.setState({ cow });
+    };
+ 
+    handleContentChange = (content = ' ') => {
+        this.setState({ content });
+    };
+
     render() {
+        const { content, cows, cow, url } = this.state;
+
         return (
-            <div className="App">
-                <h1>First React</h1>
-            </div>
+            <main className="App">
+                <section>
+                <h2>Set Options</h2>
+                <Content content={content} onChange={this.handleContentChange}/>
+                </section>
+
+                <section className="cow-say">
+                    <h2>The Meme Generator</h2>
+                    <p ref={node => this.image = node}>
+                        <CowSay content={content} cow={cow} url={url}/>
+                    </p>
+                    <p>
+                        <button onClick={this.handleExport}>Export</button>
+                    </p>
+                </section>
+            </main>
         );
     };
 };
