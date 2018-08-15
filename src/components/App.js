1f3c15e743d98a6731fce4431178d5ea1ebe919a
Arthur Jen <arthurljen@gmail.com>
2018-08-15 10:46:48 -0700

    handles image rescaling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import styles from './App.css';
+import fileSaver from 'file-saver';
+import dom2image from 'dom-to-image';
 
 class App extends Component {
   state = {
+    // url: 'https://i.ytimg.com/vi/KeM_b3O8kNQ/maxresdefault.jpg',
+    // url: 'https://images.unsplash.com/photo-1534312434688-7897cc47352a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9402e911c4071ac355a45deda0c8c7ff&auto=format&fit=crop&w=1951&q=80',
     url: 'https://amp.thisisinsider.com/images/5abb9e6a3216741c008b462d-750-563.jpg',
     header: 'Header Here',
     footer: 'Footer Here'
@@ -35,10 +39,11 @@ class App extends Component {
 
 function Meme({ header, footer, url }) {
   return (
-    <section>
-      <p>{header}</p>
-      <img src={url}/>
-      <p>{footer}</p>
+    <section id="meme" style={{
+      background: `url(${url}) no-repeat center / auto 500px`
+    }}>
+      <h3 id="header">{header}</h3>
+      <h3 id="footer">{footer}</h3>
     </section>
   );
 }
@@ -48,6 +53,11 @@ function Background({ url, onSelect }) {
     <label>
       Background:
       <input value={url} onChange={({ target }) => onSelect(target.value)} />
+      <input type="file" onChange={({ target }) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(target.files[0]);
+        reader.onload = () => onSelect(reader.result);
+      }}/>
     </label>
   );
 }
