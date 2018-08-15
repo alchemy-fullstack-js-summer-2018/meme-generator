34b67286683688e000e23555721f0bf05b3d7b6d
Robyn Navarro <rnavarro182@gmail.com>
2018-08-14 21:29:49 -0700

    added babel and its plugins

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,30 @@ module.exports = {
   plugins: [
     new CleanPlugin(`${path}/bundle.*.js`),
     new HtmlPlugin({ template: './src/index.html' })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  browsers: ['last 2 versions', 'chrome >= 65']
+                },
+              }],
+            ],
+            plugins: [
+              require('babel-plugin-transform-object-rest-spread'),
+              require('babel-plugin-transform-class-properties')
+            ],
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  }
 }
