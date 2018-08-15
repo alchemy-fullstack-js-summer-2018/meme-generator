d553bd0095fbb4128d3422e999eabf6760f04878
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-15 08:18:38 -0700

    Render content and begin to render image

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import styles from './App.css';
 
 class App extends Component {
@@ -7,23 +7,62 @@ class App extends Component {
     url: 'http://static.asiawebdirect.com/m/phuket/portals/kosamui-com/homepage/beaches/pagePropertiesImage/samui-beaches.jpg'
   };
 
+  handleContentChange = (content = '') => {
+    this.setState({ content });
+  };
+
   handleBackgroundChoose = (url = '') => {
     this.setState({ url });
   };
 
   render() {
-    const { url } = this.state;
+    const { url, content } = this.state;
 
     return (
       <main className={styles.app}>
         <section>
           <h1>Meme Generator</h1>
+          <Content content={content} onChange={this.handleContentChange}/>
           <Background url={url} onChoose={this.handleBackgroundChoose}/>
           <p>More text here</p>
+        </section>
+
+        <section className={styles.dankMeme}>
+          <h2>Here Be Your Dank Meme</h2>
+          <h3 ref={node => this.image = node}>
+            <AddText content={content} url={url}/>
+          </h3>
         </section>
       </main>
     );
   }
+}
+
+function Content({ content, onChange }) {
+  return (
+    <p>
+      <label>
+        Content:
+        <input
+          value={content}
+          onChange={({ target }) => onChange(target.value)}
+        />
+      </label>
+    </p>
+  );
+}
+
+function AddText({ url, content }) {
+  return (
+    <Fragment>
+      <p style={
+        {
+          background: `url(${url}) no-repeat center center fixed`,
+          backgroundSize: '100%'
+        }
+      }>{content}</p>
+    </Fragment>
+  );
 }
 
 function Background({ url, onChoose }) {
