262633270527c27ead806696951190e26cd36e43
carriecarter <carrie@carrie-carter.com>
2018-08-15 11:39:36 -0700

    image is displaying again, but squashed

---
+++ components/App.js
@@ -0,0 +1,68 @@
+import React, { Component, Fragment } from 'react';
+// import dom2image from 'dom-to-image';
+// import fileSaver from 'file-saver';
+import styles from '../styles/main.css';
+
+class App extends Component {
+
+  state = {
+    content: 'Meme Generator',
+    memes: [],
+    meme: 'default',
+    url: 'https://images.pexels.com/photos/416202/pexels-photo-416202.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+  };
+
+  handleContentChange = ({ target }) => {
+    this.setState({
+      content: target.value
+    });
+  };
+
+  handleBackgroundChoose = (url = '') => {
+    this.setState({ url });
+  };
+
+  render() {
+    const { content, url } = this.state;
+
+    return (
+      <main>
+        <Fragment>
+          <h1>Meme Generator</h1>
+          <input value={content} onChange={this.handleContentChange}/>
+          <Background url={url} onChoose={this.handleBackgroundChoose}/>
+        </Fragment>
+
+        <Fragment>
+          <h2>Your Meme:</h2>
+          <div>
+            <YourMeme content={content} url={url}/>
+          </div>
+        </Fragment>
+      </main>
+    );
+  }
+
+  
+}
+
+function YourMeme({ content, url }) {
+  return (
+    <Fragment>
+      <div style={{ background: `url(${url})` }}>{content}
+      </div>
+    </Fragment>
+  );
+}
+
+function Background({ url, onChoose }) {
+  return (
+    <label>
+          Background:
+      <input value={url} onChange={({ target }) => onChoose(target.value)}/>
+          
+    </label>
+  );
+}
+
+export default App;
