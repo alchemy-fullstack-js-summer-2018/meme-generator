5812a0593996e79c5f9120dda2f916d84ce56533
Robyn Navarro <rnavarro182@gmail.com>
2018-08-14 19:59:23 -0700

    webpack-cli configuration and build added to scripts

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+const { resolve } = require('path');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `./${buildDir}`
+  }
+}
