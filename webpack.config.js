55acdd81c915d1db06c58542e73d237c5e747be5
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-14 17:33:15 -0700

    Installed webpack-dev-server Step

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 const { resolve } = require('path');
-const CleanPlugin = require('clean-webpack-plugin');
-const HtmlPlugin = require('html--webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
@@ -20,6 +20,9 @@ module.exports = {
     new HtmlPlugin({ template: './src/index.html' })
   ],
   
+  devServer: {
+    contentBase: './docs',
+},
 
 
  
