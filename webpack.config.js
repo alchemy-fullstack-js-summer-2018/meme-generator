5c369f9fd2efecd5fcd3fba79b1ac2cb7992a0c2
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-14 17:59:58 -0700

    Added Babel for ESNext Step Completed

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,75 @@ module.exports = {
     publicPath: ''
   },
 
-  plugins: [
-    new CleanWebpackPlugin(`${path}/bundle.*.js`),
-    new HtmlPlugin({ template: './src/index.html' })
-  ],
-  
   devServer: {
     contentBase: './docs',
 },
 
 
- 
+  plugins: [
+    new CleanWebpackPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
+  ],
+  
+  module: {
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  browsers: 'Chrome 65'
+                  // browsers: ['last 2 versions', 'safari >= 7']
+                },
+              }],
+              'react'
+            ],
+            plugins: [
+              require('babel-plugin-transform-object-rest-spread'),
+              require('babel-plugin-transform-class-properties'),
+            ],
+            cacheDirectory: true
+          }
+        }
+      },
+    
+
+      // css
+    //   {
+    //     test: /\.css$/,
+    //     use: [
+    //       {
+    //         loader: 'style-loader',
+    //         options: { sourceMap: true }
+    //       },
+    //       {
+    //         loader: 'css-loader',
+    //         options: { 
+    //           sourceMap: true,
+    //           importLoaders: 1 
+    //         }
+    //       },
+    //       {
+    //         loader: 'postcss-loader',
+    //         options: { sourceMap: true }
+    //       }
+    //     ]
+    //   },
+
+    //   // images
+    //   {
+    //     test: /\.(jpg|png|svg)$/,
+    //     use: {
+    //       loader: 'url-loader',
+    //       options: { limit: 1000 },
+    //     },
+    //   }
+    ]
+  }
+      
 }
