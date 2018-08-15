3fea6ab783bb533a4fd31bc55e94ea0d3dca9872
Mark Myers <markalope@gmail.com>
2018-08-14 23:20:14 -0700

    sdet up react, added JSX - it works!!

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
                   // browsers: ['last 2 versions', 'safari >=7']
                 },
               }],
+              'react'
             ]
           }
         }
