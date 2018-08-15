52fff4cc068996b0cf2bf11eb245af759a52ae11
Arthur Jen <arthurljen@gmail.com>
2018-08-15 08:58:36 -0700

    states updating

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import styles from './App.css';
 
 class App extends Component {
+  state = {
+    content: 'test'
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      content: target.value
+    });
+  };
 
   render() {
+    const { content } = this.state;
+
     return (
       <main className={styles.app}>
-        <h1>Does this work?</h1>
-        <p>sure hope so</p>
+        <h1>Meme Generator</h1>
+        <p>{content}</p>
+        <input value={content} onChange={this.handleChange}/>
       </main>
     );
   }
