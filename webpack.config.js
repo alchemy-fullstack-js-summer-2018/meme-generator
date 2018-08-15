df2b1bd097048d09e2f6e15c51adff8d33d1b29b
Robyn Navarro <rnavarro182@gmail.com>
2018-08-14 23:00:51 -0700

    added css with style loader

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,26 @@ module.exports = {
             cacheDirectory: true
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          }
+        ]
       }
     ]
   }
