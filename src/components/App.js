aa3b9fb313db6f5624a496020bb313146999a791
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-15 07:39:09 -0700

    Add functional component for adding background

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,36 @@ import styles from './App.css';
 
 class App extends Component {
   state = {
-    url: ''
+    content: 'What a great beach',
+    url: 'http://static.asiawebdirect.com/m/phuket/portals/kosamui-com/homepage/beaches/pagePropertiesImage/samui-beaches.jpg'
+  };
+
+  handleBackgroundChoose = (url = '') => {
+    this.setState({ url });
   };
 
   render() {
+    const { url } = this.state;
+
     return (
       <main className={styles.app}>
         <section>
-          <h1>Hello World!</h1>
+          <h1>Meme Generator</h1>
+          <Background url={url} onChoose={this.handleBackgroundChoose}/>
           <p>More text here</p>
         </section>
       </main>
     );
   }
+}
+
+function Background({ url, onChoose }) {
+  return (
+    <label>
+      Background:
+      <input value={url} onChange={({ target }) => onChoose(target.value)}/>
+    </label>
+  );
 }
 
 export default App;
