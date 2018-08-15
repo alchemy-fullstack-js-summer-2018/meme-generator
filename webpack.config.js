2276995efbea3bf0148a1f8ab52b05437134d3c6
carriecarter <carrie@carrie-carter.com>
2018-08-14 17:18:12 -0700

    build with mode production

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 /* eslint-env node */
 const { resolve } = require('path');
-const CleanPlugin = require('clean-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+// const CleanPlugin = require('clean-webpack-plugin');
+// const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
 
 module.exports = {
 
-    entry: './src/index.js';
+  entry: './src/index.js',
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development'
+
 };
