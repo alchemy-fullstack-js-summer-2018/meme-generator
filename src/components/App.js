80bc7eb3f56eee76eded286c99d8d16a282ce6a9
MariahAdams <mariah.adams23@yahoo.com>
2018-08-15 09:53:49 -0700

    added url to state

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import '../styles/main.css';
 class App extends Component {
 
   state = {
-    content: 'Hello State!'
+    content: 'Hello State!',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIPS9VeFtecRovTEZmWFWl6RtmscJLkz7a6carZ9-IqY0ZOGPJwA'
   }
 
   handleContentChange = ({ target }) => {
@@ -12,14 +13,24 @@ class App extends Component {
     });
   };
 
+  handleBackgroundChoose = (url = '') => {
+    this.setState({ url })
+  };
+
   render() {
-    const { content } = this.state;
+    const { content, url, onChoose } = this.state;
 
     return (
       <main>
         <section>
           <h2>Content you are looking for</h2>
           <input value={content} onChange={this.handleContentChange}/>
+          <input value={url} onChange={this.handleBackgroundChoose}/>
+          <input type="file" onChange={({ target }) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(target.files[0]);
+            reader.onload = () => onChoose(reader.result);
+          }}/>
         </section>
       </main>
     );
