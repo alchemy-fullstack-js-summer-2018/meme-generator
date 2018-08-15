abfd7379c7104234b6f7da7d701914e74f3cb9c4
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-15 08:54:30 -0700

    Added React Chg code for JSX Successfully

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+
+   
+
+render() { 
+  
+  return ( 
+    <main >
+      <section>
+        <h2>Hello! Carmen here!</h2>/>
+      </section>
+
+      
+
+    </main>
+  );
+}
+
+}
+
+ 
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,9 @@ module.exports = {
     publicPath: ''
   },
 
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: './docs',
+    contentBase: `./${buildDir}`,
 },
 
 
@@ -53,36 +54,7 @@ module.exports = {
       },
     
 
-      // css
-    //   {
-    //     test: /\.css$/,
-    //     use: [
-    //       {
-    //         loader: 'style-loader',
-    //         options: { sourceMap: true }
-    //       },
-    //       {
-    //         loader: 'css-loader',
-    //         options: { 
-    //           sourceMap: true,
-    //           importLoaders: 1 
-    //         }
-    //       },
-    //       {
-    //         loader: 'postcss-loader',
-    //         options: { sourceMap: true }
-    //       }
-    //     ]
-    //   },
-
-    //   // images
-    //   {
-    //     test: /\.(jpg|png|svg)$/,
-    //     use: {
-    //       loader: 'url-loader',
-    //       options: { limit: 1000 },
-    //     },
-    //   }
+      
     ]
   }
       
