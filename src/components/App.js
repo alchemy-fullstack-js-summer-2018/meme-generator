9e811458d318cd39b86308ea590748f20ca55874
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-15 11:26:38 -0700

    Initial Meme Text Showing

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,47 @@
 import React, { Component, Fragment } from 'react';
+import styles from './App.css';
+
 
 class App extends Component {
 
-   
+  state ={
+    content: 'I luv REAL Food!',
+    url: 'http://www.diycraftsguru.com/wp-content/uploads/2017/03/ultimate-real-food-eating-guide-and-recipes.jpg'
+  };
+
+  handleContentChange = (content = ' ') => {
+    this.setState({ content});
+  };
+  
 
 render() { 
+
+  const { content, url } = this.state;
   
   return ( 
-    <main >
+    <main className={styles.app}>
       <section>
-        <h2>Hello! Carmen here!</h2>
+        <h2>Meme Project</h2>
+        <Content content={content} onChange={this.handleContentChange}/>
       </section>
-
-      
 
     </main>
   );
 }
+}
+
+function Content({ content, onChange }) {
+  return (
+    <p>
+      <label>
+        Content:
+        <input
+          value={content}
+          onChange={({ target }) => onChange(target.value)}
+        />
+      </label>
+    </p>
+  );
 
 }
 
