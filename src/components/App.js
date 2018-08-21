fc87fbf92645a66f547e8f29440564c39195532b
Mark Myers <markalope@gmail.com>
2018-08-20 17:12:28 -0700

    finishing up app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,48 @@
-// import React, { Component, Fragment } from 'react';
-// import dom2image from 'dom-to-image';
-// import fileSaver from 'file-saver';
+import React, { Component, Fragment } from 'react';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
+import styles from '.App.css';
 
-// class App extends Component {
+class App extends Component {
 
-//   state = {
-//     content: 'Enter Text Here',
-//     url: 'http://worldwidewalrusweb.com'
-//   };
+  state = {
+    content: 'Enter Text Here',
+    url: 'http://scienceblogs.com/zooillogix/wp-content/blogs.dir/253/files/2012/04/i-dd768d762eee901e4c484efece641756-walrus_baby_blingbyjlmhires08.JPG'
+  };
 
-//   handleContentChange = (content = '') => {
-//     this.setState({ content });
-//   };
+  handleContentChange = (content = '') => {
+    this.setState({ content });
+  };
 
-//   handleBackgroundChoose = (url = '') => {
-//     this.setState({ url });
-//   };
+  handleBackgroundChoose = (url = '') => {
+    this.setState({ url });
+  };
   
-//   handleExport = () => {
-//     dom2image.toBlob(this.image)
-//       .then(blob => {
-//         fileSaver.saveAs(blob, 'meme-image.png');
-//       });
-//   };
+  handleExport = () => {
+    dom2image.toBlob(this.image)
+      .then(blob => {
+        fileSaver.saveAs(blob, 'meme-image.png');
+      });
+  };
 
-//   render() {
-//     const { content, url } = this.state;
-//   }
-// }
+  render() {
+    const { content, url } = this.state;
+
+return (
+      <main className={styles.app}>
+        <section>
+          <h2>Set Options</h2>
+          <Content content={content} onChange={this.handleContentChange} />
+          <Background url={url} onChoose={this.handleBackgroundChoose} />
+        </section>
+
+        <section className="walrus-sez">
+          <h2>The Walrus Sez</h2>
+          <p ref={node => this.image = node}>
+            <WalrusSez content={content} url={url}/>
+          </p>
+        </section>
+      </main>
+    );
+  }
+}
