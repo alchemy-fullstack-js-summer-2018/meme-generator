a3e75ffa37de26b323b55c7614dfe4e3390aafd5
Rehmsy <sarahrehmer@gmail.com>
2018-08-21 19:57:36 -0700

    content header renders from app js

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
 import styles from './App.css';
 
 class App extends Component {
   state = {
-    content: 'hi ya dummy',
+    contentHeader: 'hi ya dummy',
     url: 'https://atlantisbahamas.com/media/Things%20To%20Do/Water%20Park/Beaches/Widget/Beaches_CoveBeach.jpg'
   };
 
-  render() {
-    const { content } = this.state;
+handleHeaderChange = ({ target }) => {
+  this.setState({
+    contentHeader: target.value
+  })
+}
 
-    return (
-      <main>
-        <section>
-          <h2>is i showing?? app.js</h2>
+render() {
+  const { contentHeader } = this.state;
 
-        </section>
-      </main>
-    );
-  }
+  return (
+    <main className={styles.app}>
+      <section>
+        <h2>is i showing?? app.js</h2>
+        <label>Meme:<input value={contentHeader}/></label>
+
+      </section>
+    </main>
+  );
+}
 }
 
 export default App;
