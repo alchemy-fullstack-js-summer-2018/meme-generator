4addf2b3ed731e0dd8af44a5c472f885a66e3e5f
Rehmsy <sarahrehmer@gmail.com>
2018-08-21 23:05:17 -0700

    font color working

--- src/components/app.js
+++ src/components/app.js
@@ -3,66 +3,86 @@ import dom2image from 'dom-to-image';
 import fileSaver from 'file-saver';
 import styles from './App.css';
 
-
 class App extends Component {
+  
   state = {
-    contentHeader: 'hi ya dummy',
-    contentFooter: 'hey hey hey',
-    color: 'blue',
+    contentHeader: 'that feeling when',
+    contentFooter: 'you get out of bed to eat a night popsicle',
+    color: 'white',
     textSize: '8',
-    url: 'https://atlantisbahamas.com/media/Things%20To%20Do/Water%20Park/Beaches/Widget/Beaches_CoveBeach.jpg'
+    url: 'https://www.austinchronicle.com/binary/27db/dolls.jpg'
   };
 
-handleHeaderChange = ({ target }) => {
-  this.setState({
-    contentHeader: target.value
-  });
-};
-
-handleFooterChange = ({ target }) => {
-  this.setState({
-    contentFooter: target.value
-  });
-};
-
-handleExport = () => {
-  const meme = document.getElementById('meme');
-  dom2image.toBlob(meme)
-    .then(blob => {
-      fileSaver.saveAs(blob, 'dat-meme.png');
+  handleHeaderChange = ({ target }) => {
+    this.setState({
+      contentHeader: target.value
     });
-};
+  };
 
-render() {
-  const { contentHeader, contentFooter, color, textSize, url } = this.state;
+  handleBgChoose = (url = '') => {
+    this.setState({ url });
+  };
 
-  return (
-    <main className={styles.app}>
-      <section>
-        <h2>is i showing?? app.js</h2>
-        <label>Meme:<input value={contentHeader} onChange={this.handleHeaderChange}/></label>
-        <label>Add image (400px) url:<Background url={url} onChoose={this.handleBgChoose}/></label>
-      </section>
+  handleColorChange = ({ target }) => {
+    this.setState({ color: target.value });
+  };
 
-      <section className="meme-say">
-        <h3> your meme</h3>
-        <div>
-          <MemeSays contentHeader={contentHeader} contentFooter={contentFooter} url={url} color={color} textSize={textSize}/>
-        </div>
-        <p>
-          <button onClick={this.handleExport}>SAVE</button>
-        </p>
-      </section>
-    </main>
-  );
-}
+  handleTextChange = ({ target }) => {
+    this.setState({ textSize: target.value });
+  };
+
+  handleColorChange = ({ target }) => {
+    this.setState({ color: target.value });
+  };
+
+  handleFooterChange = ({ target }) => {
+    this.setState({
+      contentFooter: target.value
+    });
+  };
+
+  handleExport = () => {
+    const meme = document.getElementById('meme');
+    dom2image.toBlob(meme)
+      .then(blob => {
+        fileSaver.saveAs(blob, 'dat-meme.png');
+      });
+  };
+
+  render() {
+    const { contentHeader, contentFooter, color, textSize, url } = this.state;
+
+    return (
+      <main className={styles.app}>
+        <section className="input-container">
+          <h1>Make Yo Meme</h1><br/>
+          <label>Top Text:<input value={contentHeader} onChange={this.handleHeaderChange}/></label>
+          <label>Bottom Text:<input value={contentFooter} onChange={this.handleFooterChange}/></label>
+          <label>Pick font color:<input type="color" value={color} onChange={this.handleColorChange}/></label>
+          <label>Add image url: <Background url={url} onChoose={this.handleBgChoose}/></label>
+        </section>
+
+        <section className="meme-say">
+          <div id="yourmeme">
+            <h4>Your Meme:</h4>
+          </div>
+          <div>
+            <MemeSays contentHeader={contentHeader} contentFooter={contentFooter} url={url} color={color} textSize={textSize}/>
+          </div>
+          <p>
+            <button onClick={this.handleExport}>SAVE</button>
+          </p>
+        </section>
+      </main>
+    );
+  }
 }
 
 function MemeSays({ url, contentHeader, contentFooter, color, textSize }) {
   return (
     <div id="meme" className="meme-container" style={{ background: `url(${url}) no-repeat` }}>
-      <h2 id="meme-header" style={{ color }}><font size={textSize}>{contentHeader}</font></h2>
-      <h3 id="meme-footer" style={{ color }}><font size={textSize}>{contentFooter}</font></h3>
+      <h3 id="meme-header" style={{ color }}><font size={textSize}>{contentHeader}</font></h3>
+      <h2 id="meme-footer" style={{ color }}><font size={textSize}>{contentFooter}</font></h2>
     </div>
   );
 }
