c9f87ab4c2cc8d45f0619b1f0cfa26413d472fd0
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-23 11:34:57 -0700

    Home Page displaying successfully

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
 import styles from './App.css';
 
 
@@ -7,14 +9,8 @@ class App extends Component {
   state ={
     content: 'I luv REAL Food!',
     url: 'https://www.castlighthealth.com/wp-content/uploads/2018/03/HealthyFood.jpg'
-    // url: 'https://bloximages.newyork1.vip.townnews.com/wtxl.com/content/tncms/assets/v3/editorial/f/4c/f4cfe044-2124-11e8-ba4e-933042f59627/5a9e66614e64b.image.jpg?resize=1200%2C630'
-    // url: 'https://media.eterritoire.fr/?u=aHR0cDovL2N0LWR3LmF5YWxpbmUuY29tOjgwL2ZpY2hpZXJzL2ZpY2hlcy9waG90b3MvNTg5MDE4NjEtbGVzLWxlZ3VtZXMtZXQtbGVzLWZydWl0cy1zdXItZm9uZC1ydXN0aXF1ZS1lbi1ib2lzLWNvcGllLWVzcGFjZS5qcGc~'
-    // url: 'http://www.diycraftsguru.com/wp-content/uploads/2017/03/ultimate-real-food-eating-guide-and-recipes.jpg'
-    // url: 'https://life.spartan.com/wp-content/uploads/2018/03/wholefood.jpg'
-    // url: 'https://img.posterlounge.co.uk/images/wbig/poster-naturkost-1346435.jpg'
-    // url: 'https://img.posterlounge.co.uk/images/wbig/poster-naturkost-1346435.jpg'
   };
-
+  
   handleContentChange = (content = ' ') => {
     this.setState({ content  });
   };
@@ -22,11 +18,18 @@ class App extends Component {
   handleBackgroundChoose = (url = '') => {
     this.setState({ url });
   };
-
-  render() { 
-
-    const { content, url } = this.state;
   
+  handleExport = () => {
+    dom2image.toBlob(this.image)
+      .then(blob => {
+        fileSaver.saveAs(blob, 'cute-realfood.png');
+      });
+  };
+  
+  render() { 
+    
+    const { content, url } = this.state;
+    
     return ( 
       <main className={styles.app}>
         <section>
@@ -37,6 +40,11 @@ class App extends Component {
 
         <section className="text">
           <Meme content={content} url={url}/>
+          <p ref={node => this.image = node}>
+          </p>
+          <p>
+            <button onClick={this.handleExport}>Export</button>
+          </p>
         </section>
 
       </main>
@@ -80,8 +88,15 @@ function Content({ content, onChange }) {
       </label>
     </p>
   );
-
+  
 }
 
- 
-export default App;
+
+export default App;   
+
+// url: 'https://bloximages.newyork1.vip.townnews.com/wtxl.com/content/tncms/assets/v3/editorial/f/4c/f4cfe044-2124-11e8-ba4e-933042f59627/5a9e66614e64b.image.jpg?resize=1200%2C630'
+// url: 'https://media.eterritoire.fr/?u=aHR0cDovL2N0LWR3LmF5YWxpbmUuY29tOjgwL2ZpY2hpZXJzL2ZpY2hlcy9waG90b3MvNTg5MDE4NjEtbGVzLWxlZ3VtZXMtZXQtbGVzLWZydWl0cy1zdXItZm9uZC1ydXN0aXF1ZS1lbi1ib2lzLWNvcGllLWVzcGFjZS5qcGc~'
+// url: 'http://www.diycraftsguru.com/wp-content/uploads/2017/03/ultimate-real-food-eating-guide-and-recipes.jpg'
+// url: 'https://life.spartan.com/wp-content/uploads/2018/03/wholefood.jpg'
+// url: 'https://img.posterlounge.co.uk/images/wbig/poster-naturkost-1346435.jpg'
+// url: 'https://img.posterlounge.co.uk/images/wbig/poster-naturkost-1346435.jpg'
